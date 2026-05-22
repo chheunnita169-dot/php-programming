efac3cbe83df1346d8368085ae167f5d110006e9
nita <chheunnita169@gmail.com>
2026-05-22 09:28:34 +0700

    commit test

---
+++ test.php
@@ -0,0 +1,9 @@
+<h1>Test coding Programming</h1>
+
+
+<?php 
+  $name ="nita";
+  $class ="WMAD";
+  echo "Dashboard  <br> ";
+  echo "Welcome back, $name !You are currently enrolled in $class (WMAD).";
+?>
